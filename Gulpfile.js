b18c28434e941f75ec15cb5b75624ae33fd493cb
Samuel Cole <sam@samuelcole.name>
2016-08-22 21:37:44 -0400

    Some developer happiness: npm start
    starts a little express server with livereload and gulp watchers that recompile
    sass
    
    also added eslint for javascript syntax checks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,40 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var bowerSrc    = require('gulp-bower-src');
-var uglify      = require('gulp-uglify');
+const child = require('child_process');
+const fs = require('fs');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const bowerSrc = require('gulp-bower-src');
+const livereload = require('gulp-livereload');
 
-gulp.task('styles', function() {
+gulp.task('server', () => {
+  const server = child.spawn('node', ['server.js']);
+  const log = fs.createWriteStream('server.log', {
+    flags: 'a',
+  });
+  server.stdout.pipe(log);
+  server.stderr.pipe(log);
+});
+
+gulp.task('styles', () => {
   gulp.src('assets/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/css/'));
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(livereload());
 });
 
-
-gulp.task('default', function() {
+gulp.task('bower', () => {
   bowerSrc()
-        .pipe(gulp.dest('assets/vendor'));
-  gulp.watch('assets/sass/*.scss', ['styles']);
+    .pipe(gulp.dest('assets/vendor'))
+    .pipe(livereload());
 });
+
+gulp.task('watch', () => {
+  livereload.listen();
+  gulp.watch('assets/sass/*.scss', ['styles']);
+  gulp.watch('assets/vendor', ['bower']);
+  gulp.watch('index.html', () => {
+    gulp.src('index.html')
+      .pipe(livereload());
+  });
+});
+
+gulp.task('default', ['server', 'watch']);
